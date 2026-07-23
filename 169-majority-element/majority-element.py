f9504218a7a1624dafd207cb564dd18efd7e0cd3
Suryamadhab Moharana <147189262+suryamadhab-m@users.noreply.github.com>
2026-07-23 12:26:48 +0530

    Time: 18 ms (14.24%) | Memory: 13.7 MB (38.85%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for n in nums:
+            d[n]=d.get(n, 0)+1
+            if d[n]>len(nums)//2:
+                return n
